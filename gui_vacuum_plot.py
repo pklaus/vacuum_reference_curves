7070990bf25dd5b3140b828e4bdce6e58a488e20
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2019-09-17 14:22:26 +0200

    nice new TimeAxisItem

--- gui_vacuum_plot.py
+++ gui_vacuum_plot.py
@@ -2,9 +2,50 @@ import PyQt5
 from PyQt5 import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
+from datetime import datetime as dt
 
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
+
+def format_timedelta(seconds):
+    return str(dt.utcfromtimestamp(seconds) - dt.utcfromtimestamp(0))
+
+class TimeAxisItem(pg.AxisItem):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.autoSIPrefix = False
+
+    def tickStrings(self, values, scale, spacing):
+        return [format_timedelta(value) for value in values]
+        # PySide's QTime() initialiser fails miserably and dismisses args/kwargs
+
+    def tickSpacing(self, minVal, maxVal, size):
+        """
+        http://www.pyqtgraph.org/documentation/_modules/pyqtgraph/graphicsItems/AxisItem.html#AxisItem.tickSpacing
+        """
+        seconds_difference = maxVal - minVal
+        if seconds_difference < 10:
+            return [ (1, 0), (1, 0) ]
+        elif seconds_difference < 60:
+            return [ (30, 0), (5, 0) ]
+        elif seconds_difference < 500:
+            return [ (60, 0), (10, 0) ]
+        elif seconds_difference < 3500:
+            return [ (15*60, 0), (60, 0) ]
+        elif seconds_difference < 11*3600:
+            return [ (3600, 0), (15*60, 0) ]
+        elif seconds_difference < 23*3600:
+            return [ (12*3600, 0), (3600, 0) ]
+        elif seconds_difference < 6*24*3600:
+            return [ (24*3600, 0), (6*3600, 0) ]
+        elif seconds_difference < 13*24*3600:
+            return [ (7*24*3600, 0), (24*3600, 0) ]
+        elif seconds_difference < 170*24*3600:
+            return [ (28*24*3600, 0), (7*24*3600, 0) ]
+        elif seconds_difference < 360*24*3600:
+            return [ (180*24*3600, 0), (7*24*3600, 0) ]
+        else:
+            return [ (365*24*3600, 0), (28*24*3600, 0) ]
 
 class VacuumPlot(pg.PlotWidget):
     """
@@ -15,13 +56,16 @@ class VacuumPlot(pg.PlotWidget):
     current_plots = {}
 
     def __init__(self, *args, **kwargs):
+
+        kwargs['axisItems'] = {'bottom': TimeAxisItem(orientation='bottom')}
+
         super().__init__(*args, **kwargs)
 
         self.setLogMode(y=True)
         self.showGrid(x=True, y=True, alpha=0.2)
 
         self.setLabel('left', 'pressure', units='mbar')
-        self.setLabel('bottom', 'Time', units='min')
+        self.setLabel('bottom', 'Time since start of pumping', units='h:mm:ss')
 
     def show_data(self, id, rc=None, c=(200, 200, 100)):
         #if id in self.current_plots: plot = self.current_plots[id]
@@ -29,7 +73,7 @@ class VacuumPlot(pg.PlotWidget):
         plot = self.current_plots.get(id, self.plot())
         self.current_plots[id] = plot
         plot.setPen(width=3, color=c)
-        xd = [(ts-rc.start) / 60 for ts in rc.data[0]]
+        xd = [(ts-rc.start) for ts in rc.data[0]]
         yd = rc.data[1]
         plot.setData(y=yd, x=xd)
 
